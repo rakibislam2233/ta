938b17d1c326b606681cad2181a51b2dc887d96c
Md Rakib Ali <rakib2020.tkg@gmail.com>
2026-01-19 12:58:31 +0600

    fix: Update PublicHome component

--- components/pages/PublicHome.tsx
+++ components/pages/PublicHome.tsx
@@ -1,10 +1,8 @@
 "use client";
-import ExampleForm from "@/components/ExampleForm";
 import PostCard from "@/components/FeedPost";
 import { MOCK_POSTS } from "@/lib/data";
 
 export default function PublicHome() {
-
   return (
     <section className="w-full max-w-2xl mx-auto px-4 py-8 flex flex-col gap-12 pb-32">
       <div className="space-y-8">
